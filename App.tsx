87dbd3257d92e87e88875b1f49129412f74f6071
Truong Huy <155792752+HuyVLT@users.noreply.github.com>
2025-06-18 14:52:02 +0700

    add navigator and searchscreen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { store } from './src/redux/store';
-import AppNavigator from './src/navigation/AppNavigator';
+import TabNavigator from './src/navigation/TabNavigator';
 
 export default function App() {
-  return (
-    <Provider store={store}>
-      <AppNavigator />
-    </Provider>
-  );
+  return <TabNavigator />;
 }
